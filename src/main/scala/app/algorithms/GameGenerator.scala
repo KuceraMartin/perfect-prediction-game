a4e5b9934de8b180ecaa4556da804db1ac0d3235
Martin Kucera <3159068+KuceraMartin@users.noreply.github.com>
2021-03-07 09:09:12 +0100

    GameGenerator

---
+++ src/main/scala/app/algorithms/GameGenerator.scala
@@ -0,0 +1,26 @@
+package app.algorithms
+
+import scala.util.Random
+
+class GameGenerator (random: Random) {
+
+  def apply[T](rows: Seq[T], cols: Seq[T]): Game[T] = {
+    val payoffs = (1 to (rows.length * cols.length)).toList
+    val rowPayoffs = random.shuffle(payoffs)
+    val colPayoffs = random.shuffle(payoffs)
+    Game((for {
+      r <- rows.indices
+      row = for {
+        c <- cols.indices
+        i = r * cols.length + c
+      } yield (cols(c), Payoff(rowPayoffs(i), colPayoffs(i)))
+    } yield (rows(r), row.toMap)).toMap)
+  }
+
+}
+
+object GameGenerator {
+
+  def apply(random: Random) = new GameGenerator(random)
+
+}
---
+++ src/test/scala/app/algorithms/GameGeneratorSpec.scala
@@ -0,0 +1,51 @@
+package app.algorithms
+
+import app.UnitSpec
+
+import scala.util.Random
+
+class GameGeneratorSpec extends UnitSpec {
+
+  "GameGenerator" should "generate a 2x2 game" in {
+    val random = new Random(1)
+    val generate = GameGenerator(random)
+    val game = generate(List(1, 2), List(1, 2))
+    game.matrix should be(Map(
+      1 -> Map(
+        1 -> Payoff(4, 4),
+        2 -> Payoff(1, 1),
+      ),
+      2 -> Map(
+        1 -> Payoff(2, 3),
+        2 -> Payoff(3, 2),
+      ),
+    ))
+  }
+
+  it should "generate a 3x4 game" in {
+    val random = new Random(1)
+    val generate = GameGenerator(random)
+    val game = generate('A' to 'C', 'D' to 'G')
+    game.matrix should be(Map(
+      'A' -> Map(
+        'D' -> Payoff(6, 6),
+        'E' -> Payoff(1, 11),
+        'F' -> Payoff(5, 3),
+        'G' -> Payoff(4, 8),
+      ),
+      'B' -> Map(
+        'D' -> Payoff(12, 12),
+        'E' -> Payoff(3, 7),
+        'F' -> Payoff(9, 9),
+        'G' -> Payoff(2, 5),
+      ),
+      'C' -> Map(
+        'D' -> Payoff(11, 1),
+        'E' -> Payoff(8, 4),
+        'F' -> Payoff(7, 10),
+        'G' -> Payoff(10, 2),
+      ),
+    ))
+  }
+
+}
