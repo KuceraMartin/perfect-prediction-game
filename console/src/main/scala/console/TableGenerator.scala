1387c3dea8a19630c27e45178ff6d6736c7d03bf
Martin Kucera <3159068+KuceraMartin@users.noreply.github.com>
2021-04-23 18:19:02 +0200

    TableGenerator special chars in constants

--- console/src/main/scala/console/TableGenerator.scala
+++ console/src/main/scala/console/TableGenerator.scala
@@ -7,12 +7,24 @@ object TableGenerator {
   private val color = "\u001b[32m"
   private val colorReset = "\u001b[0m"
 
+  private val leftTop = "┌"
+  private val innerTop = "┬"
+  private val rightTop = "┐"
+  private val leftInner = "├"
+  private val rightInner = "┤"
+  private val innerInner = "┼"
+  private val leftBottom = "└"
+  private val innerBottom = "┴"
+  private val rightBottom = "┘"
+  private val horizontal = "─"
+  private val vertical = "│"
+
 
   def create(table: Seq[Seq[String]], highlightRow: Option[Int] = None, highlightCol: Option[Int] = None): Table = {
     val colSize = table.transpose.map(_.map(_.length).max + 2)
-    val header = colSize.map("─".repeat).mkString("┌", "┬", "┐\n")
-    val sep = colSize.map("─".repeat).mkString("\n├", "┼", "┤\n")
-    val footer = colSize.map("─".repeat).mkString("\n└", "┴", "┘")
+    val header = colSize.map(horizontal.repeat).mkString(leftTop, innerTop, rightTop + "\n")
+    val sep = colSize.map(horizontal.repeat).mkString("\n" + leftInner, innerInner, rightInner + "\n")
+    val footer = colSize.map(horizontal.repeat).mkString("\n" + leftBottom, innerBottom, rightBottom)
     val centered = table.map(_.zip(colSize).map {
       case (cell: String, len: Int) =>
         val pad = len - cell.length
@@ -25,7 +37,7 @@ object TableGenerator {
       case (Some(row), Some(col)) => boldCell(hcol, row, col)
       case _ => hcol
     }
-    val string = hcell.map(_.mkString("│", "│", "│")).mkString(header, sep, footer)
+    val string = hcell.map(_.mkString(vertical, vertical, vertical)).mkString(header, sep, footer)
     Table(string, colSize.sum, hcol.size * 2 + 1)
   }
 
